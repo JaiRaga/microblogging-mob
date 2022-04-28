405da482101a0ec77119bfd12b65a6dbf679bf7b
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-28 18:12:55 +0530

    :sparkles: finished bottom tabs, created header for holding profile pic, tweet icon and user fleets

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,9 +1,12 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Entypo, Ionicons } from "@expo/vector-icons";
+import { Entypo, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
 import Home from "../components/Home";
 import Search from "../components/Search";
+import Notification from "../components/Notification";
+import Messages from "../components/Messages";
+import ProfilePicture from "../components/ProfilePicture/ProfilePicture";
 
 const BottomTab = createBottomTabNavigator();
 
@@ -13,13 +16,36 @@ export default function BottomTabNavigation() {
     <BottomTab.Navigator
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
-        tabBarShowLabel: false
+        tabBarShowLabel: false,
       }}
     >
       <BottomTab.Screen
         name="Home"
         component={Home}
         options={{
+          headerTitle: () => (
+            <Ionicons name="logo-twitter" size={30} color={Colors.light.tint} />
+          ),
+          headerTitleAlign: "center",
+          headerLeftContainerStyle: {
+            marginLeft: 15,
+          },
+          headerLeft: () => (
+            <ProfilePicture
+              size={40}
+              image="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg"
+            />
+          ),
+          headerRightContainerStyle: {
+            marginRight: 15,
+          },
+          headerRight: () => (
+            <MaterialCommunityIcons
+              name="star-four-points-outline"
+              size={24}
+              color={Colors.light.tint}
+            />
+          ),
           tabBarIcon: ({ color }) => (
             <Entypo name="home" size={24} color={color} />
           ),
@@ -36,16 +62,20 @@ export default function BottomTabNavigation() {
       />
       <BottomTab.Screen
         name="Notification"
-        component={Search}
+        component={Notification}
         options={{
           tabBarIcon: ({ color }) => (
-            <Ionicons name="ios-notifications-outline" size={24} color={color} />
+            <Ionicons
+              name="ios-notifications-outline"
+              size={24}
+              color={color}
+            />
           ),
         }}
       />
       <BottomTab.Screen
         name="Messages"
-        component={Search}
+        component={Messages}
         options={{
           tabBarIcon: ({ color }) => (
             <Ionicons name="ios-mail" size={24} color={color} />
