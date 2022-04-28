588df1c4bec134c01f74ca432b318f84c5bc61a3
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-28 18:06:53 +0530

    :zap: mock data

---
+++ assets/data/usersWithFleets.js
@@ -0,0 +1,101 @@
+export default [
+  {
+    id: "0",
+    image: "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg",
+    name: "Raga Jai Santhosh",
+    username: "JaiRaga",
+    createdAt: "2020-09-04T12:05:39.955Z",
+    fleets: {
+      items: [
+        {
+          id: "0",
+          type: "IMAGE",
+          createdAt: "2020-11-04T12:05:39.955Z",
+          image:
+            "https://images.pexels.com/photos/235734/pexels-photo-235734.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+        {
+          id: "1",
+          type: "TEXT",
+          text: " Hi, there!Hi, there!Hi, there!Hi, there!Hi, there!Hi, there!Hi, there!Hi, there!Hi, there!",
+        },
+      ],
+    },
+  },
+  {
+    id: "1",
+    image:
+      "https://images.pexels.com/photos/1887624/pexels-photo-1887624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Luke",
+    username: "Lu",
+    createdAt: "2020-09-04T12:05:39.955Z",
+    fleets: {
+      items: [
+        {
+          id: "0",
+          type: "IMAGE",
+          image:
+            "https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      ],
+    },
+  },
+  {
+    id: "2",
+    image:
+      "https://images.pexels.com/photos/532168/pexels-photo-532168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Daniil",
+    username: "Daniil",
+    createdAt: "2020-09-04T12:05:39.955Z",
+    fleets: {
+      items: [
+        {
+          id: "0",
+          type: "TEXT",
+          text: "I am from Kaliningrad",
+        },
+      ],
+    },
+  },
+  {
+    id: "3",
+    image:
+      "https://images.pexels.com/photos/4014919/pexels-photo-4014919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Alex",
+    username: "Alexa",
+    createdAt: "2020-09-04T12:05:39.955Z",
+    fleets: {
+      items: [
+        {
+          id: "0",
+          type: "IMAGE",
+          image:
+            "https://images.pexels.com/photos/133081/pexels-photo-133081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+      ],
+    },
+  },
+  {
+    id: "4",
+    image:
+      "https://images.pexels.com/photos/163891/italy-sicily-sea-ocean-163891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Elon Musk",
+    username: "Musk",
+    createdAt: "2020-09-04T12:05:39.955Z",
+    fleets: {
+      items: [
+        {
+          id: "0",
+          type: "IMAGE",
+          image:
+            "https://images.pexels.com/photos/2448749/pexels-photo-2448749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+        {
+          id: "1",
+          type: "TEXT",
+          text: "Of Course I Still Love You",
+        },
+      ],
+    },
+  },
+];
