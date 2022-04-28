944efd5850ec18cf2db0c0d578cc36b3ce0f2a28
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-28 18:09:23 +0530

    :sparkles: displays individual tweet item

---
+++ components/Tweet/Tweet.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import LeftContainer from "./LeftContainer";
+import MainContainer from "./MainContainer";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+
+const Tweet = ({ tweet }) => {
+  return (
+    <View style={styles.container}>
+      {/* Left container */}
+      <LeftContainer user={tweet.user} />
+
+      {/* Main container */}
+      <MainContainer tweet={tweet} />
+    </View>
+  );
+};
+
+export default Tweet;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    // flexWrap: "wrap",
+    padding: 10,
+    borderBottomWidth: 0.6,
+    borderBottomColor: "grey",
+    // backgroundColor: "blue"
+  }
+});
