3554e5f44c11e7d7b5f9db363cf9a73fec2bf3f7
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-29 06:28:08 +0530

    :art: added styling for images and icons

--- components/Tweet/MainContainer/MainContainer.js
+++ components/Tweet/MainContainer/MainContainer.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
+import moment from "moment"
+import Footer from "./Footer";
 
 const MainContainer = ({ tweet }) => {
   return (
@@ -10,23 +12,25 @@ const MainContainer = ({ tweet }) => {
         <View style={styles.tweetHeaderText}>
           <Text style={styles.name}>{tweet.user.name}</Text>
           <Text style={styles.username}>@{tweet.user.username}</Text>
-          <Text style={styles.createdAt}>15s</Text>
+          <Text style={styles.dot}>&#8226;</Text>
+          <Text style={styles.createdAt}>{moment(tweet.createdAt).fromNow()}</Text>
         </View>
-          <Ionicons
-            name="chevron-down"
-            style={styles.showMoreIcon}
-            size={16}
-            color="grey"
-          />
+        <Ionicons
+          name="chevron-down"
+          style={styles.showMoreIcon}
+          size={16}
+          color="grey"
+        />
       </View>
 
       {/* content */}
       <View style={styles.contentcontainer}>
         <Text style={styles.content}>{tweet.content}</Text>
-        {!!tweet.image && <Image source={{ uri: tweet.image }} />}
+        {!!tweet.image && <Image style={styles.image} source={{ uri: tweet.image }} />}
       </View>
 
       {/* tweet footer */}
+      <Footer tweet={tweet} />
     </View>
   );
 };
@@ -36,27 +40,32 @@ export default MainContainer;
 const styles = StyleSheet.create({
   tweetContainer: {
     flex: 1,
+    marginHorizontal: 10
     // backgroundColor: "red",
   },
   tweetHeaderContainer: {
     // backgroundColor: "lime",
     flexDirection: "row",
-  justifyContent: "space-between",
+    justifyContent: "space-between",
     width: "100%",
   },
   tweetHeaderText: {
     flexDirection: "row",
   },
   name: {
-    marginHorizontal: 5,
+    marginRight: 5,
     fontWeight: "bold",
   },
   username: {
-    marginHorizontal: 5,
+    marginRight: 5,
     color: "grey",
   },
+  dot: {
+    marginRight: 5,
+    color: "grey"
+  },
   createdAt: {
-    marginHorizontal: 5,
+    marginRight: 5,
     color: "grey",
   },
   showMoreIcon: {
@@ -66,6 +75,16 @@ const styles = StyleSheet.create({
     // backgroundColor: "green",
   },
   content: {
+    marginTop: 5,
+    lineHeight: 18
     // backgroundColor: "pink",
   },
+  image: {
+    marginVertical: 10,
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+    borderRadius: 15,
+    overflow: "hidden"
+  }
 });
