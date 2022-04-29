5a92c4c59b6c9f2c07988e7d5365bd4b372c5344
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-29 06:24:49 +0530

    :zap: created screen for new tweet

--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -1,14 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NewTweetScreen from "../screen/NewTweetScreen";
 import BottomTabNavigation from "./BottomTabNavigator";
-import Home from "../components/Home";
-import Search from "../components/Search";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigation} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigation}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NewTweet"
+        component={NewTweetScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
