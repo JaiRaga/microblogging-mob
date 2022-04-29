3c2b580950582d8a94c01314ab8dd61d2e63767f
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-29 06:23:44 +0530

    :sparkles: created screen for creating new tweet

---
+++ screen/NewTweetScreen.js
@@ -0,0 +1,96 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import Colors from "../constants/Colors";
+import { TouchableOpacity } from "react-native";
+import ProfilePicture from "../components/ProfilePicture";
+
+const NewTweetScreen = () => {
+  const [tweet, setTweet] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const onPostTweet = () => {
+    console.warn(`Posting ${tweet} and image ${imageUrl}`);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <AntDesign name="close" size={30} color={Colors.light.tint} />
+        <TouchableOpacity style={styles.button} onPress={onPostTweet}>
+          <Text style={styles.buttonText}>Tweet</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.newTweetContainer}>
+        <ProfilePicture image="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg" />
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={tweet}
+            onChangeText={setTweet}
+            multiline={true}
+            numberOfLines={3}
+            style={styles.tweetInput}
+            placeholder="What's happening?"
+          />
+          <TextInput
+            value={imageUrl}
+            onChangeText={setImageUrl}
+            style={styles.imageInput}
+            placeholder="Image url(Optional)"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NewTweetScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "flex-start",
+    marginTop: StatusBar.currentHeight,
+  },
+  headerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15,
+  },
+  button: {
+    backgroundColor: Colors.light.tint,
+    borderRadius: 30,
+  },
+  buttonText: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  newTweetContainer: {
+    flexDirection: "row",
+    padding: 15,
+  },
+  inputContainer: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  tweetInput: {
+    fontSize: 20,
+    // backgroundColor: "yellow",
+    // alignItems: "flex-start",
+    // padding: 5
+    // height: 100,
+    // maxHeight: 300,
+  },
+  imgeInput: {},
+});
