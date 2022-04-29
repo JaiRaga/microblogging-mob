a8fca8b309f35e9e123580536fe873943b15df6c
JaiRaga <raga.j.santhosh@gmail.com>
2022-04-29 06:28:56 +0530

    :sparkles: displays likes, retweets, share and comments details

---
+++ components/Tweet/MainContainer/Footer/Footer.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Feather, EvilIcons, AntDesign } from "@expo/vector-icons";
+
+const Footer = ({ tweet }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <Feather name="message-circle" size={20} color="grey" />
+        <Text style={styles.numbers}>{tweet.numberOfComments}</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <EvilIcons name="retweet" size={30} color="grey" />
+        <Text style={styles.numbers}>{tweet.numberOfRetweets}</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <AntDesign name="hearto" size={20} color="grey" />
+        <Text style={styles.numbers}>{tweet.numberOfLikes}</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <EvilIcons name="share-google" size={28} color="grey" />
+      </View>
+    </View>
+  );
+};
+
+export default Footer;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 5
+    // backgroundColor: "yellow"
+  },
+  iconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    // backgroundColor: "yellow"
+  },
+  numbers: {
+    color: "grey",
+    marginLeft: 5,
+    textAlign: "center"
+  },
+});
